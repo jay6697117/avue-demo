dce8252b402b3bde07ed5fe78f5cce05339663bb
steve zhang <871506263@qq.com>
2021-12-27 21:35:12 +0800

    提交代码于: 2021-12-27 21:35:12

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,49 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <avue-crud :data="data" :option="option" v-model="form"></avue-crud>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {},
+      data: [
+        {
+          name: '张三',
+          sex: '男'
+        },
+        {
+          name: '李四',
+          sex: '女'
+        },
+        {
+          name: '王五',
+          sex: '女'
+        },
+        {
+          name: '赵六',
+          sex: '男'
+        }
+      ],
+      option: {
+        align: 'center',
+        menuAlign: 'center',
+        column: [
+          {
+            label: '姓名',
+            prop: 'name'
+          },
+          {
+            label: '性别',
+            prop: 'sex'
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
